fb852e39736071aacca56f6fab635df4d9ad1841
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-03-13 14:50:52 -0400

    fixed: text in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <div className="hero">
       <div className="hero-content">
         <h1>Welcome to our Logistics Services</h1>
-        <p>Professional logistics consulting and service provided by veterans in teh industry</p>
+        <p>Professional logistics consulting and service provided by veterans in the industry</p>
         <a href="#contact" className="hero-btn">Schedule Service</a>
       </div>
     </div>
